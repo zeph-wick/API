0997adab88e09a1420877b4b6e1583bc6d1d3e63
zeph-wick <irakozezephylin2016@gmail.com>
2022-12-31 02:02:44 +0200

    Update config.php

--- config.php
+++ config.php
@@ -6,7 +6,7 @@ require './autoload.php';
 
 // For test payments we want to enable the sandbox mode. If you want to put live
 // payments through then this setting needs changing to `false`.
-$enableSandbox = true;
+$enableSandbox = false;
 
 // PayPal settings. Change these to your account details and the relevant URLs
 // for your site.
